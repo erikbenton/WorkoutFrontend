0aa1f0329727c7570833d6db9687af0e730d47d0
Erik Benton <erik.benton@hotmail.com>
2025-10-26 17:15:35 -0700

    adding Ref for checking that useFetch only sets state if the component is mounted

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,29 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 const baseUrl = import.meta.env.VITE_BASE_API_URL
 
 const useFetch = (url) => {
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMounted.current = true;
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if (isMounted.current) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e)
+        if (isMounted.current) setError(e);
       } finally {
-        setLoading(false)
+        if (isMounted.current) setLoading(false);
       }
     }
     init();
+
+    return () => isMounted.current = false;
   }, [url])
 
   return { data, error, loading }
