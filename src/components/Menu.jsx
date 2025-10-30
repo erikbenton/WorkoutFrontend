0c070b08ecb3759bc39623b70aaecd8b5438b49e
Erik Benton <erik.benton@hotmail.com>
2025-10-30 11:32:10 -0700

    moving running workout elapsed time to the far right of NavBar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,39 +11,43 @@ const Menu = () => {
   };
 
   return (
-    <Navbar className="d-flex" collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#" to="/" as="span">
-            <Link style={padding} to="/">Home</Link>
-          </Nav.Link>
-          <Nav.Link href="#" to="/Programs" as="span">
-            <Link style={padding} to="/Programs">Programs</Link>
-          </Nav.Link>
-          <Nav.Link href="#" to="/Workouts" as="span">
-            <Link style={padding} to="/Workouts">Workouts</Link>
-          </Nav.Link>
-          <Nav.Link href="#" to="/Exercises" as="span">
-            <Link style={padding} to="/Exercises">Exercises</Link>
-          </Nav.Link>
-          <Nav.Link href="#" to="/completedWorkouts" as="span">
-            <Link style={padding} to="/completedWorkouts">Workout History</Link>
-          </Nav.Link>
-          <Nav.Link href="#" as="span">
-            {(!match || !runningWorkout)
-              && <Link style={padding} to="/runningWorkout">
-                {runningWorkout ? "Continue Workout" : "Do a Workout"}
-              </Link>
-            }
-          </Nav.Link>
-          {runningWorkout && (
-            <Nav.Item className="justify-content-end">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" to="/" as="span">
+              <Link style={padding} to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" to="/Programs" as="span">
+              <Link style={padding} to="/Programs">Programs</Link>
+            </Nav.Link>
+            <Nav.Link href="#" to="/Workouts" as="span">
+              <Link style={padding} to="/Workouts">Workouts</Link>
+            </Nav.Link>
+            <Nav.Link href="#" to="/Exercises" as="span">
+              <Link style={padding} to="/Exercises">Exercises</Link>
+            </Nav.Link>
+            <Nav.Link href="#" to="/completedWorkouts" as="span">
+              <Link style={padding} to="/completedWorkouts">Workout History</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {(!match || !runningWorkout)
+                && <Link style={padding} to="/runningWorkout">
+                  {runningWorkout ? "Continue Workout" : "Do a Workout"}
+                </Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Nav>
+      <Nav className="ms-auto me-5">
+        {runningWorkout && (
+            <Nav.Item>
               <ElapsedTimer startTime={Date.parse(runningWorkout.startTime)} />
             </Nav.Item>
           )}
-        </Nav>
-      </Navbar.Collapse>
+      </Nav>
     </Navbar>
   )
 }
