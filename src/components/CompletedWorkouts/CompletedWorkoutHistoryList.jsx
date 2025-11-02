fc304652e341b0cc957d901e04412c1dfb3c470d
Erik Benton <erik.benton@hotmail.com>
2025-11-02 08:06:10 -0800

    first attempt at styling Workout History list with bootstrap

--- src/components/CompletedWorkouts/CompletedWorkoutHistoryList.jsx
+++ src/components/CompletedWorkouts/CompletedWorkoutHistoryList.jsx
@@ -13,13 +13,40 @@ const CompletedWorkoutHistoryList = () => {
     return dateNoTime;
   }
 
+  const formattedDuration = (duration) => {
+    const [hours, minutes, seconds] = duration.split(":");
+    const hoursText = hours === "00"
+      ? ""
+      : `${hours}hr `;
+    const minutesText = minutes === "00"
+      ? ""
+      : `${minutes}min `;
+    const secondsText = seconds === "00"
+      ? ""
+      : `${seconds}s`;
+
+    return `${hoursText.replace(/^0/, "")}${minutesText.replace(/^0/, "")}${secondsText.replace(/^0/, "")}`;
+  }
+
   return (
     <div>
       <h2>Workout Histories</h2>
       <ul>
         {completedWorkouts.map(workout =>
           <li key={workout.id}>
-            <Link to={`/completedWorkouts/${workout.id}`}>{workout.name} - {getFormatedCompletion(workout)}</Link>
+            <div className="card my-2 p-2 col-md-8 col-lg-5">
+              <div className="row">
+                <Link
+                  to={`/completedWorkouts/${workout.id}`}
+                >
+                  {workout.name} - {getFormatedCompletion(workout)}
+                </Link>
+              </div>
+              <div className="row">
+                <p className="card-text mb-1">{workout.numberOfExerciseGroups} exercises completed</p>
+                <p className="card-text">Duration: {formattedDuration(workout.duration)}</p>
+              </div>
+            </div>
           </li>
         )}
       </ul>
