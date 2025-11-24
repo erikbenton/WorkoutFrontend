0ef968bd9ea4449f7f42f694f74517810da39372
Erik Benton <erik.benton@hotmail.com>
2025-11-24 10:16:04 -0800

    deleting a completed wokout is now a modal prompt

--- src/components/CompletedWorkouts/CompletedWorkoutDetails.jsx
+++ src/components/CompletedWorkouts/CompletedWorkoutDetails.jsx
@@ -2,12 +2,17 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { Button } from "react-bootstrap";
 import completedWorkoutService from "../../services/completedWorkout";
+import { useDispatch, useSelector } from "react-redux";
+import { MODAL_TYPES, setModal } from "../../reducers/modals";
+import ConfirmModal from "../Modals/ConfirmModal";
 
 
 const CompletedWorkoutDetails = () => {
   const id = Number(useParams().id);
   const navigate = useNavigate();
   const { data: completedWorkout, loading, error } = useFetch(`completedWorkouts/${id}`);
+  const dispatch = useDispatch();
+  const modals = useSelector(state => state.modals);
 
   if (loading) return <h1>Loading workout...</h1>
   if (error) return <h1>error</h1>
@@ -17,23 +22,33 @@ const CompletedWorkoutDetails = () => {
     return dateNoTime;
   }
 
-  const deleteWorkout = async (workout) => {
-    const response = confirm(`Delete workout ${workout.name}?`);
-    if (response) {
-      try {
-        await completedWorkoutService.remove(workout.id)
-        navigate("/completedWorkouts");
-      } catch (error) {
-        console.error(error);
-      }
+  const promptToDelete = () => {
+    dispatch(setModal({ openModal: MODAL_TYPES.DELETE_WORKOUT }))
+  }
+
+  const deleteWorkout = async () => {
+    try {
+      await completedWorkoutService.remove(completedWorkout.id)
+      navigate("/completedWorkouts");
+    } catch (error) {
+      console.error(error);
     }
   }
+
+  const showDeleteModal = modals === MODAL_TYPES.DELETE_WORKOUT;
 
   const totalReps = group => group.completedExerciseSets.reduce((acc, curr) => acc + curr.reps, 0);
   const totalWeight = group => group.completedExerciseSets.reduce((acc, curr) => acc + (curr.weight ?? 0), 0);
 
   return (
     <div className="container">
+      {showDeleteModal &&
+        <ConfirmModal
+          show={showDeleteModal}
+          header="Delete workout"
+          body="Delete this workout record?"
+          confirmModal={deleteWorkout}
+        />}
       <h1>{completedWorkout.name} - {getFormatedCompletion(completedWorkout)}</h1>
       <p>Duration: {completedWorkout.duration}</p>
       <ol className="no-bullets">
@@ -72,7 +87,7 @@ const CompletedWorkoutDetails = () => {
         variant="danger"
         type="button"
         className="ms-1"
-        onClick={() => deleteWorkout(completedWorkout)}
+        onClick={promptToDelete}
       >
         Delete?
       </Button>
